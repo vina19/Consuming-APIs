1437302575ea0a7d1cb79e35a778c0c27ed44b79
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-10 15:02:52 -0500

    Create the API mock test

--- test_bitcoin.py
+++ test_bitcoin.py
@@ -6,4 +6,16 @@ import Bitcoin
 
 class testBitcoin(TestCase):
 
-    @pactch('builtins.input', side_effect=)
+    @patch('Bitcoin.convert_value_to_USD')
+    def test_value_to_USD(self, mock_rates):
+        mock_rate = 12.34567
+        example_api_response = {'bpi': {'USD': {'code': 'USD', 'symbol': '&#36;', 'rate': {mock_rate}, 'description': 'United States Dollar', 'rate_float' : '8569.8667'}}}
+        mock_rates.side_effect = [ example_api_response ]
+
+        converted = Bitcoin.convert_value_to_USD(100, 'rate')
+        self.assertEqual(8,569,866.700, converted)
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
