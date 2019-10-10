b9954085a6f573ca77efbbe12547c976c53f704f
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-10 15:02:10 -0500

    Seperate the get API rate with converting to USD function

--- Bitcoin.py
+++ Bitcoin.py
@@ -2,33 +2,42 @@ import requests
 
 def main():
 
+    user_input = get_value_bitcoin()
+    rate_value = get_API_rate_data()
+    convert_value = convert_value_to_USD(user_input, rate_value)
+    display_result = display_value_in_USD(user_input, rate_value)
+
+# Get the user input
+def get_value_bitcoin():
+
+    user_input = int(input('Enter your Bitcoin number: '))
+    
+    return user_input
+
+# Fetch the data
+def get_API_rate_data():
+
+    # Using requests to get the bitcoin API in json format
     data = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json').json()
-
-    user_input = int(input('Enter a number of Bitcoin: '))
-
+    
     rate = data['bpi']['USD']['rate']
+
+    return rate
+
+# Conversion to USD value
+def convert_value_to_USD(user_input, rate):
 
     user_USD_value = user_input * rate
 
-    print(f'The rate is: {rate}, The user Bitcoin value in USD is: $ ' + user_USD_value)
+    return user_USD_value
+
+# Displaying the result
+def display_value_in_USD(user_input, rate):
+
+    user_USD_value = convert_value_to_USD(user_input, rate)
+
+    print(f'The user Bitcoin value in USD is: $ ' + user_USD_value)
+
     
-    # forecast_data = data['list']
-
-    # print('Weather Forecast \n')
-
-    # # Fetch data needed
-    # for forecast in forecast_data:
-    #     timestamp = forecast['dt']
-    #     date = datetime.fromtimestamp(timestamp)
-    #     weather_description = forecast['weather'][0]['description']
-    #     temp_f = forecast['main']['temp']
-    #     wind_speed = forecast['wind']['speed']
-    #     print(f'Date: {date}')
-    #     print(f'Weather description: {weather_description}')
-    #     print(f'Temperature: {temp_f:.2f} F') 
-    #     print(f'Wind speed: {wind_speed} \n')
-
 if __name__ == '__main__':
     main()
-
-
