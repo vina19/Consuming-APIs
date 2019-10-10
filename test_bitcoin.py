4a8af6377a31154f6518ab07c22f308c90049b8e
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-10 15:38:52 -0500

    Create the API mock test

--- test_bitcoin.py
+++ test_bitcoin.py
@@ -12,7 +12,7 @@ class testBitcoin(TestCase):
         example_api_response = {'rate': mock_rate}
         mock_rates.side_effect = [ example_api_response ]
         value = Bitcoin.get_value_bitcoin()
-        expected = value * mock_rate
+        expected = Bitcoin.convert_value_to_USD(value, mock_rate)
 
         converted = Bitcoin.convert_value_to_USD(mock_rate, value)
         self.assertEqual(expected, converted)
