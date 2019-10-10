5621cad4f516a9dad1dfde9706fff10998b0eef9
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-10 15:31:03 -0500

    Fix the test value to USD function

--- test_bitcoin.py
+++ test_bitcoin.py
@@ -6,14 +6,16 @@ import Bitcoin
 
 class testBitcoin(TestCase):
 
-    @patch('Bitcoin.convert_value_to_USD')
+    @patch('Bitcoin.get_API_rate_data')
     def test_value_to_USD(self, mock_rates):
-        mock_rate = 12.3456
+        mock_rate = 123456
         example_api_response = {'rate': mock_rate}
         mock_rates.side_effect = [ example_api_response ]
+        value = Bitcoin.get_value_bitcoin()
+        expected = value * mock_rate
 
-        converted = Bitcoin.convert_value_to_USD(2, mock_rate)
-        self.assertEquals(246.912, converted)
+        converted = Bitcoin.convert_value_to_USD(mock_rate, value)
+        self.assertEqual(expected, converted)
 
 if __name__ == '__main__':
     unittest.main()
